13c0d9dbc267af2a87472669a3f9af71f125d6fc
Dave2048IT <129850145+Dave2048IT@users.noreply.github.com>
2023-04-08 23:32:06 +0200

    Funktion für Akku-Abfrage angepasst

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
 (function() {
-    const plugInSound = new Audio('water-droplets-1.wav');
-    const unplugSound = new Audio('matches-1.wav');
-  
-    function updateBatteryInfo() {
-      navigator.getBattery().then(battery => {
-        const chargingEl = document.getElementById("charging");
-        const levelEl = document.getElementById("level");
-        const reminderEl = document.getElementById("reminder");
-        
-        chargingEl.textContent = battery.charging ? "Charging" : "Not Charging";
-        levelEl.textContent = `${Math.floor(battery.level * 100)} %`;
-        
-        if (battery.charging && battery.level > 0.75) {
+  const chargingEl = document.getElementById("charging");
+  const levelEl = document.getElementById("level");
+  const reminderEl = document.getElementById("reminder");
+  const plugInSound = new Audio('audio/water-droplets-1.wav');
+  const unplugSound = new Audio('audio/matches-1.wav');
+
+  function onBatteryStatusChange(status) {
+      const isCharging = status.charging;
+      const batteryLevel = Math.floor(status.level * 100);
+
+      chargingEl.innerText = isCharging ? "Charging" : "Not Charging";
+      levelEl.innerText = `${batteryLevel}%`;
+
+      const color = [
+          Math.floor(255 - (255 / 100) * batteryLevel),
+          Math.floor((255 / 100) * batteryLevel),
+          0
+      ];
+
+
+      if (isCharging && batteryLevel > 75) {
           unplugSound.play();
-          document.body.style.backgroundColor = "darkgoldenrod";
-          reminderEl.textContent = "It's hot. Please unplug the charger.";
-        }
-        else if (!battery.charging && battery.level < 0.18) {
+          reminderEl.innerText = "It's hot. Please unplug the charger.";
+      } else if (!isCharging && batteryLevel < 18) {
           plugInSound.play();
-          document.body.style.backgroundColor = "darkblue";
-          reminderEl.textContent = "I am thirsty. Please plug in the charger.";
-        }
-        else {
-          reminderEl.textContent = "";
-          document.body.style.backgroundColor = "black";
-        }
-      });
-    }
-  
-    updateBatteryInfo();
-    setInterval(updateBatteryInfo, 30000);
+          reminderEl.innerText = "I am thirsty. Please plug in the charger.";
+      } else {
+          reminderEl.innerText = "";
+      }
+
+      document.body.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+  }
+
+
+  window.addEventListener("load", async function () {
+      // Check for browser compatibility
+      const status_api = "onbatterystatus" in navigator;
+      const battery_api = "getBattery" in navigator || ("battery" in navigator && "Promise" in window);
+      if (status_api)
+          alert("Supports onbatterystatus");
+          
+      if (battery_api)
+          alert("Supports getBattery")
+          
+      if (!status_api && !battery_api) {
+          alert("Battery APIs are not supported on this browser.");
+          return;
+      }
+
+      // Get initial battery status and register levelchange event listener using async/await syntax
+      let battery = await navigator.getBattery();
+      onBatteryStatusChange(battery);
+
+      if (status_api) {
+          // Register onbatterystatus event listener if available
+          navigator.onbatterystatus = function (event) {
+              onBatteryStatusChange(event.target);
+          };
+      } else if (battery_api) {
+          // Register levelchange event listener using async/await syntax
+          battery.addEventListener("levelchange", async function () {
+              onBatteryStatusChange(battery);
+          });
+      }
+
+      // Update battery status every 30 seconds
+      setInterval(async function () {
+          battery = await navigator.getBattery();
+          onBatteryStatusChange(battery);
+      }, 120 * 1000);
+  });
 })();
